527f03336f8ce5a9ebcc3384e2da0dcebc85f5e1
Colin Pronovost <colin.pron@live.com>
2019-10-02 16:14:17 -0400

    Initial support for building with SVA

---
+++ xen/arch/x86/sva/include/stdint.h
@@ -0,0 +1,6 @@
+/**
+ * SVA compatibility - SVA headers include stdint.h, so we expose
+ * xen/types.h under that name.
+ */
+
+#include <xen/types.h>
---
+++ xen/arch/x86/sva/include/sys/types.h
@@ -0,0 +1,6 @@
+/**
+ * SVA compatibility - SVA headers include sys/types.h, so we expose
+ * xen/types.h under that name.
+ */
+
+#include <xen/types.h>
