6a6a0ee817091815eebf67a4f443d5b5b900e0ed
Colin Pronovost <colin.pron@live.com>
2019-10-16 12:38:26 -0400

    Specialize the `guest_mode` macro for SVA

--- xen/include/asm-x86/regs.h
+++ xen/include/asm-x86/regs.h
@@ -4,6 +4,8 @@
 
 #include <asm/x86_64/regs.h>
 
+#ifndef CONFIG_SVA
+
 #define guest_mode(r)                                                         \
 ({                                                                            \
     unsigned long diff = (char *)guest_cpu_user_regs() - (char *)(r);         \
@@ -14,6 +16,16 @@
     /* Return TRUE if it's a guest frame. */                                  \
     (diff == 0);                                                              \
 })
+
+#else
+
+/*
+ * SVA doesn't make use of guest_cpu_user_regs, so a user-mode interrupt context
+ * may exist somewhere other than the bottom of the stack.
+ */
+#define guest_mode(r)   (((r)->cs & 0x3) != 0)
+
+#endif
 
 #define return_reg(v) ((v)->arch.user_regs.rax)
 
