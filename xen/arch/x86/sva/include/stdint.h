f4bd8426721279fb0559a9520478e313b8abf3a3
Colin Pronovost <colin.pron@live.com>
2019-11-18 17:16:26 -0500

    Remove SVA header compatibility shims
    With the new changes to SVA, these are no longer necessary.

--- xen/arch/x86/sva/include/stdint.h
+++
@@ -1,6 +0,0 @@
-/**
- * SVA compatibility - SVA headers include stdint.h, so we expose
- * xen/types.h under that name.
- */
-
-#include <xen/types.h>
--- xen/arch/x86/sva/include/sys/types.h
+++
@@ -1,6 +0,0 @@
-/**
- * SVA compatibility - SVA headers include sys/types.h, so we expose
- * xen/types.h under that name.
- */
-
-#include <xen/types.h>
